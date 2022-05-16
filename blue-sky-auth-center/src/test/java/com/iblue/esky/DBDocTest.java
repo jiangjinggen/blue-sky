4c7abfb10e6b75180ec871290ac730c1f1d6b549
22268728@qq.com <22268728@qq.com>
2022-05-16 14:39:43 +0800

    add screw core produce db doc

--- blue-sky-auth-center/src/test/java/com/iblue/esky/DBDocTest.java
+++ blue-sky-auth-center/src/test/java/com/iblue/esky/DBDocTest.java
@@ -6,6 +6,8 @@ import cn.smallbun.screw.core.engine.EngineFileType;
 import cn.smallbun.screw.core.engine.EngineTemplateType;
 import cn.smallbun.screw.core.execute.DocumentationExecute;
 import cn.smallbun.screw.core.process.ProcessConfig;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -27,7 +30,99 @@ public class DBDocTest {
 
     @Autowired
     private ApplicationContext applicationContext;
+    /**
+     * 文档生成
+     */
+    @Test
+    public void documentGeneration() {
+        List<String> dbList = Arrays.asList("aas_onl",
+                "aps_onl",
+                "cfs_onl",
+                "cis_onl",
+                "clearing_onl",
+                "csc_onl",
+                "custody_onl",
+                "dcs_onl",
+                "ets_onl",
+                "flow_onl",
+                "mcs_onl",
+                "mds_onl",
+                "mock_onl",
+                "ofs_onl",
+                "ots_onl",
+                "rcs_onl",
+                "ssc_onl",
+                "thames_onl",
+                "uis_onl",
+                "xxl_job");
+        for(String dbname : dbList) {
+            //数据源
+            HikariConfig hikariConfig = new HikariConfig();
+            hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
+            String dbLin = "jdbc:mysql://127.0.0.1:3306/" + dbname;
+            hikariConfig.setJdbcUrl(dbLin);
+            hikariConfig.setUsername("root");
+            hikariConfig.setPassword("123456");
+            //设置可以获取tables remarks信息
+            hikariConfig.addDataSourceProperty("useInformationSchema", "true");
+            hikariConfig.setMinimumIdle(2);
+            hikariConfig.setMaximumPoolSize(5);
+            DataSource dataSource = new HikariDataSource(hikariConfig);
+            //生成配置
+            EngineConfig engineConfig = EngineConfig.builder()
+                    //生成文件路径
+                    //.fileOutputDir(fileOutputDir)
+                    .fileOutputDir("D:\\jjg\\doc\\DWh\\A3_DbDoc_20220516")
+                    //打开目录
+                    .openOutputDir(true)
+                    //文件类型
+                    .fileType(EngineFileType.HTML)
+                    //生成模板实现
+                    .produceType(EngineTemplateType.freemarker).build();
+            //自定义文件名称
+            //.fileName("自定义文件名称").build();
 
+            //忽略表
+            ArrayList<String> ignoreTableName = new ArrayList<>();
+            ignoreTableName.add("test_user");
+            ignoreTableName.add("test_group");
+            //忽略表前缀
+            ArrayList<String> ignorePrefix = new ArrayList<>();
+            ignorePrefix.add("test_");
+            //忽略表后缀
+            ArrayList<String> ignoreSuffix = new ArrayList<>();
+            ignoreSuffix.add("_test");
+            ProcessConfig processConfig = ProcessConfig.builder()
+                    //指定生成逻辑、当存在指定表、指定表前缀、指定表后缀时，将生成指定表，其余表不生成、并跳过忽略表配置
+                    //根据名称指定表生成
+                    .designatedTableName(new ArrayList<>())
+                    //根据表前缀生成
+                    .designatedTablePrefix(new ArrayList<>())
+                    //根据表后缀生成
+                    .designatedTableSuffix(new ArrayList<>())
+                    //忽略表名
+                    .ignoreTableName(ignoreTableName)
+                    //忽略表前缀
+                    .ignoreTablePrefix(ignorePrefix)
+                    //忽略表后缀
+                    .ignoreTableSuffix(ignoreSuffix).build();
+            //配置
+            Configuration config = Configuration.builder()
+                    //版本
+                    .version("1.0.0")
+                    //描述
+                    .description(dbname)
+                    //数据源
+                    .dataSource(dataSource)
+                    //生成配置
+                    .engineConfig(engineConfig)
+                    //生成配置
+                    .produceConfig(processConfig)
+                    .build();
+            //执行生成
+            new DocumentationExecute(config).execute();
+        }
+    }
     @Test
     public void buildDBDoc() {
 
